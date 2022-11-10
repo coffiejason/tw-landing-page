bf6871e769fb70fb6298d209044fb36cba458958
coffiejason <coffiejasoncj@gmail.com>
2022-11-10 20:14:41 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { About, HeroSection, Navbar, Support } from './components'
+import styles from './style'
+
+const App = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <About />
+    <Support />
+  </>
+)
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export {default as Navbar} from './Navbar/NavBar'
+export {default as HeroSection} from './Hero/Hero'
+export {default as About} from './About/About'
+export {default as Support} from './Support/Support'
